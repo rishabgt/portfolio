d38028599f14867e4fe1fb68da67e1ad663a49c6
Rishab Prasad <rishabgt@gmail.com>
2022-04-10 08:40:41 +0530

    Years of experience dynamic

--- js/script.js
+++ js/script.js
@@ -3,6 +3,12 @@ $(document).ready(function(){
         $(this).toggleClass('fa-times');
         $('header').toggleClass('toggle');
     });
+    var date1 = new Date("08/08/2020");
+    var currentDate = new Date();
+    var exp = currentDate.getTime()-date1.getTime();
+    exp = exp/(1000*3600*24*365);
+    var exp = Math.round(exp*10)/10;
+    $('.counter .box span').text(exp);
 });
 
 $(window).on('scroll load',function(){
